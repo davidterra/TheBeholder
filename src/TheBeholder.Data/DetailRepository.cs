35f131768023e834a7123da3fd1438c08ac0ac7a
David Terra Alves <david@davidterra.com>
2021-01-14 21:53:34 -0300

    Removido o código comentado.

--- src/TheBeholder.Data/DetailRepository.cs
+++ src/TheBeholder.Data/DetailRepository.cs
@@ -16,27 +16,9 @@ namespace TheBeholder.Data
             : base(dbMongo)
                 => this.Collection = dbMongo.Collection<Detail>();
 
-        public async Task<Detail> GetByTicketAsync(string ticket)
-        {
-            var result = await this.Collection.FindAsync<Detail>(filter => filter.Ticket == ticket.ToUpper());
-
-            // var minDocument = collection.Find(query).SetSortOrder(sortAscending).SetLimit(1).First();
-            // var maxDocument = collection.Find(query).SetSortOrder(sortDescending).SetLimit(1).First();
-
-            // var minDateTime = minDocument["Entry"].AsDateTime;
-            // var maxDateTime = maxDocument["Entry"].AsDateTime;
-
-            // var builder = Builders<BsonDocument>.Sort;
-            // var sort = builder.Ascending("x").Descending("y");
-
-            // var sortDescending = SortBy.Descending("Entry");
-
-            // var max = Collection.Find().SetSortOrder()
-
-            // var builder = Builders<BsonDocument>.Sort;
-            // var sort = builder.Descending("CreatedAt");
-
-           return await  Collection.Find(filter => filter.Ticket == ticket.ToUpper()).SortByDescending(sort => sort.CreatedAt).FirstOrDefaultAsync();
-        }
+        public async Task<Detail> GetByTicketAsync(string ticket) 
+            => await Collection.Find(filter => filter.Ticket == ticket.ToUpper())
+                                .SortByDescending(sort => sort.CreatedAt)
+                                .FirstOrDefaultAsync();
     }
 }
